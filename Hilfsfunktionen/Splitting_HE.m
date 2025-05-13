167ab46fc3cf9ab31182c395dc529ac6619ac275
Daniel-Heins <daniel.heins2001@gmail.com>
2025-05-13 12:41:03 +0200

    Delete Hilfsfunktionen directory

--- Hilfsfunktionen/Splitting_HE.m
+++
@@ -1,8 +0,0 @@
-%Split Lösung zu der Wärme Leitungs Gleichung mit Zeitschritt tau
-function Split_first = Splitting_HE(v_0,tau,N)
-
-    coef  = func_to_fourrier(v_0,N);
-    z     = [exp(-((pi*(0:N-1)).^2)*tau).*coef zeros(1,N)];
-    Split_first    = fourrier_to_func(z,N);
-
-end
--- Hilfsfunktionen/fourrier_to_func.m
+++
@@ -1,7 +0,0 @@
-function func = fourrier_to_func(fourrier_werte,N)
-
-    z = [fourrier_werte zeros(1,N)];
-    FFT_z = sqrt(2)*imag(FFt(z,2*N));
-    func = FFT_z(1,1:N);
-    
-end
